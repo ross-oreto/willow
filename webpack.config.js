7782ea9195d1c3289e02b1f09c206f78f13f6660
ross-oreto <ross.oreto@pm.me>
2021-11-17 13:40:56 -0600

    Add css-minimizer-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     plugins: [
@@ -11,6 +12,14 @@ module.exports = {
                 test: /\.css$/i,
                 use: [ MiniCssExtractPlugin.loader, "css-loader"]
         }],
+    },
+    optimization: {
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers
+            // (i.e. `terser-webpack-plugin`)
+            `...`,
+            new CssMinimizerPlugin(),
+        ],
     },
     resolve: {
         extensions: ['.js'],
